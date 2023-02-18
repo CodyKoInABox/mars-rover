b80527ed3a5566ebe82f896f4713eb366944ca44
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-02-18 20:54:58 -0300

    Add files via upload

--- rover.js
+++ rover.js
@@ -12,7 +12,7 @@ let rightBorder = [3, 7, 11, 15];
 let mode = false;
 //le o cache do navegador para ver se o usuario ja usou o site antes, caso tenha usado, "lembra" de qual modo o usuario usou por ultimo e tambem lembra da ultima posicao do rover
 mode = localStorage.getItem("mode");
-currentPosition = parseInt(localStorage.getItem("currentPosition"));
+//currentPosition = parseInt(localStorage.getItem("currentPosition"));
 
 
 //funcao puramente visual, ela apenas mostra ao usuario qual modo esta selecionado, se o cache do navegador indicar que o modo deve ser o B, o switch que tem na tela vai automaticamente para a posicao B
@@ -24,7 +24,10 @@ function setmode(){
         document.getElementById("modeswitch").checked = false; 
     }
 
-    if(currentPosition == "null"){
+    currentPosition = parseInt(localStorage.getItem("currentPosition"));
+    if(Number.isInteger(currentPosition) == "true"){
+    }
+    else{
         currentPosition = 0;
     }
     //"chama" a funcao enviar que faz com que o rover va para a posicao que ele estava quando a pagina foi fechada pela ultima vez (usando o cache do navegador)
